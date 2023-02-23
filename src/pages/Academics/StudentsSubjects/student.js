b2a7aa98955ffbe76a568afb673b805dea48bb6c
Your Name <bsbawlia@gmail.com>
2023-02-23 22:18:53 +0530

    commit 1

---
+++ src/pages/Academics/StudentsSubjects/student.js
@@ -0,0 +1,35 @@
+import React from "react";
+import pic from "../../../assests/schedule.jpg"
+
+import Record from './student.json';
+import './student.css';
+
+function Student(){
+
+    return(
+        <div className="time-table">
+            <h1>Your Departmental Time Schedule</h1>
+
+        <div className="inner">
+            {
+            Record && Record.map( (record) => {
+                return(
+                    <div  key={record.id}>
+                <img src={pic} />
+
+                        <br/>
+                    {/* <h3>
+                    { record.caption }
+                    </h3> */}
+                      
+                    </div>
+                )
+            }
+            )}
+        </div>
+
+        </div>  
+    )
+}
+
+export default Student;
